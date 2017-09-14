a329214959d5d4a4f9a34bbea29089d7796fa31d
dan-roentsch <dan.roentsch@mheducation.com>
2017-09-14 12:24:00 -0400

    Throw away this commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'the-god-app',
+    template: `
+<div>
+    <router-outlet></router-outlet>
+</div>
+`,
+})
+export class AppComponent {
+}
---
+++ src/app/helloworld/hello-world.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+    moduleId: module.id,
+    templateUrl: 'hello-world.component.html',
+    styleUrls: ['hello-world.component.css'],
+})
+export class HelloWorldComponent  {
+    public user: string;
+    constructor() {
+        this.user = "World";
+    }
+}
