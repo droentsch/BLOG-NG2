52fe8d5c8b1548957660227b1cd54f33b95d9549
droentsch <dan.roentsch@gmail.com>
2018-06-07 02:52:46 -0400

    Finished linear nav

--- src/app/home/linear-nav/linear-nav.component.ts
+++ src/app/home/linear-nav/linear-nav.component.ts
@@ -1,13 +1,14 @@
-import  { Component } from '@angular/core';
+import  { Component, OnInit } from '@angular/core';
 import { StateService } from '../../service/state.service';
 import { BroadcastService } from '../../service/broadcast.service';
+import { IBlogConfig } from '../../model/IBlogConfig';
 
 @Component({
     selector: 'linear-nav',
     styleUrls: ['./linear-nav.component.css', '../home.component.css'],
     templateUrl: 'linear-nav.component.html'
 })
-export class LinearNavComponent {
+export class LinearNavComponent implements OnInit {
 
     private state: StateService;
     private broadcast: BroadcastService;
@@ -21,19 +22,22 @@ export class LinearNavComponent {
         this.isLastChapter = false;
     }
 
+    public ngOnInit() {
+        this.registerBroadcasts();
+    }
+    private registerBroadcasts() {
+        this.broadcast.onConfigData()
+            .subscribe((data: IBlogConfig) => this.setFirstChapterState());
+    }
     public gotoFirstChapter() {
         const lastChap = this.getFirstChapter();
         if (lastChap !== '') {
-            this.isFirstChapter = true;
-            this.isLastChapter = false;
             this.broadcast.chapterChange(lastChap);
         }
     }
     public gotoLastChapter() {
         const lastChap = this.getLastChapter();
         if (lastChap !== '') {
-            this.isFirstChapter = false;
-            this.isLastChapter = true;
             this.broadcast.chapterChange(lastChap);
         }
     }
@@ -52,26 +56,45 @@ export class LinearNavComponent {
     private getLastChapter(): string {
         const chaps = this.state.blogConfig.chapters;
         if (chaps.length) {
+            this.setLastChapterState();
             this.state.currentChapter = chaps.length - 1;
             return chaps[this.state.currentChapter];
         }
         return '';
     }
+
     private getFirstChapter(): string {
         const chaps = this.state.blogConfig.chapters;
         if (chaps.length) {
+            this.setFirstChapterState();
             this.state.currentChapter = 0;
             return chaps[this.state.currentChapter];
         }
         return '';
     }
+
     private getChapter(num: number): string {
         const chaps = this.state.blogConfig.chapters;
         if (this.state.currentChapter + num + 1 <= chaps.length) {
             this.state.currentChapter = this.state.currentChapter + num;
+            if (this.state.currentChapter + 1 === chaps.length) {
+                this.setLastChapterState();
+            } else if (this.state.currentChapter === 0) {
+                this.setFirstChapterState();
+            }
             return chaps[this.state.currentChapter];
         }
         return '';
+    }
+
+    private setFirstChapterState() {
+        this.isFirstChapter = true;
+        this.isLastChapter = false;
+    }
+
+    private setLastChapterState() {
+        this.isFirstChapter = false;
+        this.isLastChapter = true;
     }
 }
 
