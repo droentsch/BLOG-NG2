78790b3b9a1f4cd1edb8f6301e958dcf23d3832a
dan roentsch <dan.roentsch@gmail.com>
2018-11-29 01:49:05 -0500

    Added Input() decorator in DisplayContentComponent

--- src/app/home/content-display/content-display.component.ts
+++ src/app/home/content-display/content-display.component.ts
@@ -1,13 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { SafeHtml } from '@angular/platform-browser';
 @Component({
     selector: 'content-display',
-    inputs: ['content'],
     templateUrl: './content-display.component.html',
     styleUrls: ['./content-display.component.css']
 })
 export class ContentDisplayComponent {
-    public content: SafeHtml;
+    @Input() content: SafeHtml;
 
     constructor() {
 
